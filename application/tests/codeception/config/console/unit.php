1fab10a401be922c811ec048b56b9aa8749d0cfc
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-04-07 13:11:10 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- application/tests/codeception/config/console/unit.php
+++ application/tests/codeception/config/console/unit.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Application configuration for console unit tests
- */
+     * Application configuration for console unit tests
+     */
 return yii\helpers\ArrayHelper::merge(
     require(YII_APP_BASE_PATH . '/common/config/main.php'),
     require(YII_APP_BASE_PATH . '/console/config/main.php'),
