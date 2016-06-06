2f864018c89a398f3562357908412a2eb4c13a9e
Phillip Shipley <phillip_shipley@sil.org>
2016-06-06 13:55:01 -0400

    update default zxcvbn endpoint for unit tests

--- application/common/config/local.test.php
+++ application/common/config/local.test.php
@@ -2,7 +2,7 @@
 
 use Sil\PhpEnv\Env;
 
-$zxcvbnApiBaseUrl = Env::get('ZXCVBN_API_BASEURL');
+$zxcvbnApiBaseUrl = Env::get('ZXCVBN_API_BASEURL', 'http://zxcvbn:3000');
 
 return [
     'params' => [
