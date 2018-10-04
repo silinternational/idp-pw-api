13556f424550490a4f8027ad12da8064aa4baee5
Steven Schram <steve_schram@sil.org>
2018-10-04 16:34:35 -0400

    Use whenavail when testing with broker container

--- application/run-tests.sh
+++ application/run-tests.sh
@@ -17,36 +17,34 @@ whenavail ${MYSQL_HOST} 3306 100 /data/yii migrate --interactive=0
 whenavail ${MYSQL_HOST} 3306 100 /data/yii migrate --interactive=0 --migrationPath=console/migrations-test
 
 # Install and enable xdebug for code coverage
-#apt-get install -y php5-xdebug git
-#php5enmod xdebug
+apt-get install -y php-xdebug
 
 # Run codeception tests
-#whenavail broker 80 100 echo "broker ready"
-sleep 10
-runny ./vendor/bin/codecept run unit
+whenavail broker 80 100 echo "broker ready"
+runny ./vendor/bin/codecept run unit --coverage --coverage-xml
+TESTRESULTS_UNIT=$?
 
 # Run behat tests
 runny ./vendor/bin/behat --config=tests/features/behat.yml --strict
+TESTRESULTS_BEHAT=$?
 
-##### Disabled reporting of code coverage on 3/30 because scrutinizer isnt working with it yet
+# Clone repo to get git parents
+cd /tmp
+rm -rf idp-pw-api/
+git clone https://github.com/silinternational/idp-pw-api.git
+cd idp-pw-api/
+PARENTS=`git log --pretty=%P -n 1 ${CI_COMMIT_ID}`
 
-#./vendor/bin/codecept run unit --coverage --coverage-xml
-#TESTRESULTS=$?
-#
+# Push coverage data to scrutinizer
+cd /data
+curl -Lo ocular.phar https://scrutinizer-ci.com/ocular.phar
+php ocular.phar code-coverage:upload --repository="g/silinternational/idp-pw-api" --revision="${CI_COMMIT_ID}" --parent="${PARENTS}" --format=php-clover -n -vvv tests/_output/coverage.xml
 
-## Clone repo to get git parents
-#cd /tmp
-#rm -rf idp-pw-api/
-#git clone https://github.com/silinternational/idp-pw-api.git
-#cd idp-pw-api/
-#PARENTS=`git log --pretty=%P -n 1 ${CI_COMMIT_ID}`
-#
-## Push coverage data to scrutinizer
-#cd /data
-#curl -Lo ocular.phar https://scrutinizer-ci.com/ocular.phar
-#php ocular.phar code-coverage:upload --repository="g/silinternational/idp-pw-api" --revision="${CI_COMMIT_ID}" --parent="${PARENTS}" --format=php-clover -n -vvv tests/_output/coverage.xml
-#
-## If unit tests fail, make sure to exit with error status
-#if [[ "$TESTRESULTS" -ne 0 ]]; then
-#    exit $TESTRESULTS
-#fi
+# If unit tests fail, make sure to exit with error status
+if [[ "$TESTRESULTS_UNIT" -ne 0 ]]; then
+    exit $TESTRESULTS_UNIT
+fi
+
+if [[ "$TESTRESULTS_BEHAT" -ne 0 ]]; then
+    exit $TESTRESULTS_BEHAT
+fi
