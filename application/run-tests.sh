c364e2a9bdd16930d2814602287e1f6938921634
Steven Schram <steve_schram@sil.org>
2018-10-04 16:55:29 -0400

    Disabled call to ocular.php

--- application/run-tests.sh
+++ application/run-tests.sh
@@ -28,17 +28,19 @@ TESTRESULTS_UNIT=$?
 runny ./vendor/bin/behat --config=tests/features/behat.yml --strict
 TESTRESULTS_BEHAT=$?
 
+## The ocular.php script sometimes works and sometimes hangs with no error message
+
 # Clone repo to get git parents
-cd /tmp
-rm -rf idp-pw-api/
-git clone https://github.com/silinternational/idp-pw-api.git
-cd idp-pw-api/
-PARENTS=`git log --pretty=%P -n 1 ${CI_COMMIT_ID}`
+#cd /tmp
+#rm -rf idp-pw-api/
+#git clone https://github.com/silinternational/idp-pw-api.git
+#cd idp-pw-api/
+#PARENTS=`git log --pretty=%P -n 1 ${CI_COMMIT_ID}`
 
 # Push coverage data to scrutinizer
-cd /data
-curl -Lo ocular.phar https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --repository="g/silinternational/idp-pw-api" --revision="${CI_COMMIT_ID}" --parent="${PARENTS}" --format=php-clover -n -vvv tests/_output/coverage.xml
+#cd /data
+#curl -Lo ocular.phar https://scrutinizer-ci.com/ocular.phar
+#php ocular.phar code-coverage:upload --repository="g/silinternational/idp-pw-api" --revision="${CI_COMMIT_ID}" --parent="${PARENTS}" --format=php-clover -n -vvv tests/_output/coverage.xml
 
 # If unit tests fail, make sure to exit with error status
 if [[ "$TESTRESULTS_UNIT" -ne 0 ]]; then
