9cfc9ec904be88021f19dad4fd56342ce360c5bc
Steven Schram <steve_schram@sil.org>
2018-11-27 11:26:46 -0700

    Scrutinizer feedback

--- application/frontend/controllers/UserController.php
+++ application/frontend/controllers/UserController.php
@@ -6,8 +6,6 @@ use frontend\components\BaseRestController;
 
 use yii\filters\AccessControl;
 use yii\helpers\ArrayHelper;
-use yii\web\ForbiddenHttpException;
-use yii\web\UnauthorizedHttpException;
 
 class UserController extends BaseRestController
 {
@@ -49,10 +47,10 @@ class UserController extends BaseRestController
          */
         $user = \Yii::$app->user->identity;
 
-        $do_not_disclose = \Yii::$app->request->getBodyParam('do_not_disclose');
+        $doNotDisclose = \Yii::$app->request->getBodyParam('do_not_disclose');
 
-        if ($do_not_disclose !== null) {
-            $user->do_not_disclose = $do_not_disclose ? 1 : 0;
+        if ($doNotDisclose !== null) {
+            $user->do_not_disclose = $doNotDisclose ? 1 : 0;
             $user->save();
         }
 
