04ce8753f1bf3b322c71b4023e24272fe8c45603
Schparky <3172830+Schparky@users.noreply.github.com>
2019-02-25 07:02:42 -0700

    better cleanup at shutdown

--- application/run.sh
+++ application/run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# establish a signal handler to catch the SIGTERM from a 'docker stop'
+# reference: https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86
+term_handler() {
+  apache2ctl stop
+  killall cron
+  exit 143; # 128 + 15 -- SIGTERM
+}
+trap 'kill ${!}; term_handler' SIGTERM
+
 if [[ "x" == "x$LOGENTRIES_KEY" ]]; then
     echo "Missing LOGENTRIES_KEY environment variable";
 else
@@ -7,7 +16,7 @@ else
     sed -i /etc/rsyslog.conf -e "s/LOGENTRIESKEY/${LOGENTRIES_KEY}/"
     # Start syslog
     rsyslogd
-    sleep 10
+    sleep 3
 fi
 
 # Run database migrations
@@ -18,5 +27,10 @@ if [[ $APP_ENV == "dev" ]]; then
     apt-get install php-xdebug
 fi
 
-# Run apache in foreground
-apache2ctl -D FOREGROUND
+apache2ctl start
+
+# endless loop with a wait is needed for the trap to work
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
