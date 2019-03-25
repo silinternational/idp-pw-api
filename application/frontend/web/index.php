08bd9719f87f4e513b014b91ae5faaf176fa3108
Schparky <3172830+Schparky@users.noreply.github.com>
2019-03-25 09:05:42 -0600

    only use YII_DEBUG for dev and test, not for prod or stage

--- application/frontend/web/index.php
+++ application/frontend/web/index.php
@@ -7,7 +7,7 @@ use \yii\web\ServerErrorHttpException;
 require(__DIR__ . '/../../vendor/autoload.php');
 
 define('YII_ENV', Env::get('APP_ENV', 'prod'));
-define('YII_DEBUG', YII_ENV !== 'prod');
+define('YII_DEBUG', YII_ENV === 'dev' || YII_ENV === 'test');
 
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
