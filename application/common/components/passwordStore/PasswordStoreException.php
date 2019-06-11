d0e19cbb847d638aa5ff8e98b62b0fe2c2eaa05e
Schparky <3172830+Schparky@users.noreply.github.com>
2019-06-11 11:29:56 -0600

    update exception comment

--- application/common/components/passwordStore/PasswordStoreException.php
+++ application/common/components/passwordStore/PasswordStoreException.php
@@ -2,8 +2,8 @@
 namespace common\components\passwordStore;
 
 /**
- * An exception which indicates that the operation was not attempted for some
- * reason, most likely because not all of the backends were available.
+ * An exception which indicates that the operation was not fully successful for some
+ * reason.
  */
 class PasswordStoreException extends \Exception
 {
