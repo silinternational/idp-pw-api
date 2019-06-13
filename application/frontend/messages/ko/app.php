152d0805390e570a844d5c4302e5419e76a5e3d1
Schparky <3172830+Schparky@users.noreply.github.com>
2019-06-13 16:14:34 -0500

    New translations app.php (Korean)

--- application/frontend/messages/ko/app.php
+++ application/frontend/messages/ko/app.php
@@ -36,6 +36,8 @@ return [
     'Mfa.TypeMissing' => '종류를 입력하십시오',
     'Mfa.UpdateFailure' => '업데이트 실패',
     'Mfa.VerifyFailure' => 'MFA 확인 실패',
+    'Multiple.SetPartialSuccess {successes} {errors}' => 'Successfully set the password in {successes}, but failed to set the password in {errors}',
+    'Multiple.SetFailed {errors}' => 'Failed to set the password in {errors}',
     'Password.Breached' => '입력 한 비밀번호는 이전에 다른 웹 사이트의 데이터 유출로 발견되었습니다. 귀하의 계좌가 손상되었을 수도 있고 아닐 수도 있습니다. 여기에 다른 암호를 사용하고 방문하시기 바랍니다 <a href="https://idphelp.sil.org/logging-in/password/password-recommendations#h.p_LOkQcA18P0qs" target="_blank">이 도움말 페이지</a> 자세한 내용은.',
     'Password.DisallowedContent' => '귀하의 비밀번호는 다음을 포함하지 않을 수 있습니다 : {labelList} (코드 180)',
     'Password.MissingPassword' => '비밀번호가 필요합니다.',
