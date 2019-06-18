ccb6af6a0b21d762015748b747fc8350e99b351b
Schparky <3172830+Schparky@users.noreply.github.com>
2019-06-18 12:55:47 -0600

    change to 'error' level

--- application/common/components/passwordStore/Google.php
+++ application/common/components/passwordStore/Google.php
@@ -309,7 +309,7 @@ class Google extends Component implements PasswordStoreInterface
         }
 
         if (count($response['users']) > 1) {
-            \Yii::warning([
+            \Yii::error([
                 'action' => 'getEmailFromGoogle',
                 'status' => 'too many results',
                 'employee_id' => $employeeId,
