0c855f74a25716328020c78a3ca786acadfbe7de
briskt <3172830+briskt@users.noreply.github.com>
2025-07-17 16:27:16 +0800

    remove error_reporting(E_ALL)

--- application/frontend/web/index.php
+++ application/frontend/web/index.php
@@ -2,8 +2,5 @@
 
 $config = require('../config/load-configs.php');
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 $application = new yii\web\Application($config);
 $application->run();
